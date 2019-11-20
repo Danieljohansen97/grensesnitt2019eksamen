e11e9282cda603189d41fec66d3972bfef935ff5
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-11-20 01:44:28 +0100

    Add some dummycontent to treatments

--- public/js/mobileJs/booking.js
+++ public/js/mobileJs/booking.js
@@ -3,11 +3,3 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded.");
 });
 
-// Hamburgermenu and overlay
-function openOverlay() {
-    document.getElementById("myNav").style.width = "100%";
-}
-  
-function closeOverlay() {
-    document.getElementById("myNav").style.width = "0%";
-}
---
+++ public/js/mobileJs/treatment.js
@@ -0,0 +1,21 @@
+// DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded.");
+    // Toggle treatmentBody
+    // Needs refactoring
+    document.getElementById("th1").addEventListener("click", () => {
+        document.getElementById("tb1").classList.toggle("hideBody");
+    });
+    document.getElementById("th2").addEventListener("click", () => {
+        document.getElementById("tb2").classList.toggle("hideBody");
+    });
+    document.getElementById("th3").addEventListener("click", () => {
+        document.getElementById("tb3").classList.toggle("hideBody");
+    });
+    document.getElementById("th4").addEventListener("click", () => {
+        document.getElementById("tb4").classList.toggle("hideBody");
+    });
+
+
+});
+
