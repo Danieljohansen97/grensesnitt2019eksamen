7ac1f0b4572cc030dce57cfa4b87e1bcb60e1192
theaholme <thea.aastad@gmail.com>
2019-11-20 19:16:39 +0100

    add webJs and add overlay solution for web

---
+++ public/js/webJs/index.js
@@ -0,0 +1,8 @@
+// Hamburgermenu overlay
+function openOverlay() {
+    document.getElementById("myNav").style.width = "25%";
+}
+  
+function closeOverlay() {
+    document.getElementById("myNav").style.width = "0%";
+}
