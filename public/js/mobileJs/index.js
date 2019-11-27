78b0780c79a555473dfbcc705a852719de9b96ed
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-11-27 01:20:50 +0100

    Make user functionality available on all pages

--- public/js/mobileJs/index.js
+++ public/js/mobileJs/index.js
@@ -1,3 +1,41 @@
+// Get the modals and buttons
+var userModal = document.getElementById("userModal");
+var userModalBtn = document.getElementById("userModalBtn");
+var loginModal = document.getElementById("loginModal");
+var loginModalBtn2 = document.getElementById("loginModalBtn2");
+var registerModal = document.getElementById("registerModal");
+
+// Get the <span> element that closes the modal
+var userClose = document.getElementsByClassName("close")[0];
+var loginClose = document.getElementsByClassName("close")[1];
+var registerClose = document.getElementsByClassName("close")[2];
+
+// When the user clicks on the button, open the modal
+userModalBtn.onclick = function() {
+  userModal.style.display = "block";
+  loginModal.style.display = "none";
+  registerModal.style.display = "none";
+}
+
+loginModalBtn2.onclick = function() {
+  loginModal.style.display = "block";
+  registerModal.style.display = "none";
+  userModal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+userClose.onclick = function() {
+  userModal.style.display = "none";
+}
+loginClose.onclick = function() {
+  loginModal.style.display = "none";
+}
+registerClose.onclick = function() {
+  registerModal.style.display = "none";
+}
+
+
+// Logged in or logged out setup
 const loggedOutContent = document.querySelectorAll(".loggedOut");
 const loggedInContent = document.querySelectorAll(".loggedIn");
 const userDetails = document.querySelector("#userDetails");
@@ -29,8 +67,6 @@ const setupUI = (user) => {
 // Hamburgermenu overlay
 function openOverlay() {
     document.getElementById("myNav").style.width = "40%";
-    
-  
 }
   
 function closeOverlay() {
